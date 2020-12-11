28593d1e718bb49de4852c860cd0e231bcc76e2c
Gabriel Nori <gabrielnori@gabrielnori.tech>
2020-12-10 21:25:18 -0300

    It works!!

--- IotGarden.ino
+++ IotGarden.ino
@@ -1,4 +1,6 @@
 #include "./WifiCredential.h"
+#include "./MqttCredential.h"
+#include "DHT.h"
 #include <Arduino.h>
 #include <PubSubClient.h> //Library to handle MQTT
 #include <ESP8266WiFi.h>
@@ -6,13 +8,13 @@
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
-#define plant1Sensor 4
-#define plant2Sensor 5
-#define plant3Sensor 6
-#define plant4Sensor 7
-#define rainSensor 1
-#define dhtSensor 3
-#define led 2
+#define plant1Sensor 5
+#define plant2Sensor 14
+#define plant3Sensor 12
+#define plant4Sensor 13
+#define rainSensor 10
+#define dhtSensor 4
+#define led 16
 #define waterTankSenor A0
 
 #define DHTTYPE DHT11
@@ -21,6 +23,26 @@ DHT dht(dhtSensor, DHTTYPE);
 long otaTimer = 0;
 
 String chipName = "IoTGarden";
+
+bool debug = false;
+
+long
+	lastMqttTry = 0,
+	mqttRetryTime = 0;
+
+MqttCredential mqttCredential;
+WiFiClient netClient;
+PubSubClient mqttClient(netClient);
+
+const char* mqttServer = mqttCredential.mqttServer;
+const char* mqttUser = mqttCredential.mqttUser;
+const char* mqttPassword = mqttCredential.mqttPassword;
+const char* outTopic = mqttCredential.outTopic;
+const char* inTopic = mqttCredential.inTopic;
+const int mqttPort = mqttCredential.mqttPort;
+
+void mqttCallBack(char* topic, byte* payload, unsigned int length);
+
 
 void setup() {
 	WifiCredential credential;
@@ -33,6 +55,9 @@ void setup() {
 	WiFi.begin(credential.wifiSsid, credential.wifiPassword);
 	long startWifi = millis();
 
+	WiFi.setAutoConnect(true);
+	WiFi.setAutoReconnect(true); 
+
 	while(WiFi.status() != WL_CONNECTED){
 		if(millis() - startWifi < 50000){
 			Serial.print(".");
@@ -42,11 +67,10 @@ void setup() {
 			Serial.println("\nError while connecting to WiFi...");
 			ESP.restart();
 		}
+		ESP.wdtFeed();
 	}
 
 	Serial.println("WiFiConnected");
-
-	delay(100);
 
 	ArduinoOTA.setHostname(chipName.c_str());
 	ArduinoOTA.setPort(3232);
@@ -83,10 +107,12 @@ void setup() {
 	});
   	ArduinoOTA.begin();
 
-	dht.begin();
 	Serial.println("Ready");
 	Serial.print("IP address: ");
 	Serial.println(WiFi.localIP());
+	dht.begin();
+	ESP.wdtFeed();
+
 	pinMode(led, OUTPUT);
 	pinMode(plant1Sensor, INPUT);
 	pinMode(plant2Sensor, INPUT);
@@ -94,13 +120,94 @@ void setup() {
 	pinMode(plant4Sensor, INPUT);
 	pinMode(rainSensor, INPUT);
 	pinMode(waterTankSenor, INPUT);
+	digitalWrite(led, HIGH);
+
 	delay(10);
-	digitalWrite(led, LOW);
+
+	mqttClient.setServer(mqttServer, mqttPort);
+	mqttClient.setCallback(mqttCallBack);
+	connectMqtt();
 }
 
 void loop() {
 	ArduinoOTA.handle();
+
+	if(mqttClient.connected()){
+		mqttClient.loop();
+	}
+	else{
+		reconnectMqtt();
+	}
+
 	float humidity = dht.readHumidity();
 	float temperature = dht.readTemperature();
-	float heatIndex = dht.computeHeatIndex(t, h, false);
+	float heatIndex = dht.computeHeatIndex(temperature, humidity, false);
+
+	Serial.println(F("Sending frame..."));
+
+	sendMqtt("temperature", String(temperature));
+	sendMqtt("humidity", String(humidity));
+	sendMqtt("heatindex", String(heatIndex));
+	sendMqtt("plant1", String(!digitalRead(plant1Sensor)));
+	sendMqtt("plant2", String(!digitalRead(plant2Sensor)));
+	sendMqtt("plant3", String(!digitalRead(plant3Sensor)));
+	sendMqtt("plant4", String(!digitalRead(plant4Sensor)));
+	sendMqtt("rain", String(!digitalRead(rainSensor)));
+
+	digitalWrite(led, LOW);
+	delay(100);
+	digitalWrite(led, HIGH);
+	delay(1000);
+}
+
+void mqttCallBack(char* topic, byte* payload, unsigned int length) {
+	if(debug){
+		Serial.print(F("Message arrived ["));
+		Serial.print(topic);
+		Serial.print(F("] "));
+	}
+	String payloadIn;
+	for (int i = 0; i < length; i++) {
+		payloadIn += (char)payload[i];
+	}
+	if(debug){
+		Serial.print(F("\nPayload from MQTT: " ));
+		Serial.println(payloadIn);
+	}
+	//router(payloadIn);
+}
+
+void reconnectMqtt(){
+	if(!millis() - lastMqttTry > mqttRetryTime){
+		if(debug){
+			Serial.println(F("MQTT has gone down... Reconnecting"));
+		}
+		connectMqtt();
+		lastMqttTry = millis();
+	}
+}
+
+void connectMqtt() {
+	if(debug){
+		Serial.print(F("Attempting MQTT connection..."));
+	}
+	// Attempt to connect
+	if (mqttClient.connect(chipName.c_str(), mqttUser, mqttPassword)) {
+		if(debug){
+			Serial.println(F("MQTT connected"));
+		}
+		// Once connected, publish the connect message and the current switch state
+		mqttClient.publish((String(outTopic) + "/stat").c_str(), "connected");
+		mqttClient.subscribe(inTopic);
+		digitalWrite(led, LOW);
+	} else {
+		Serial.print(F("failed, rc="));
+		Serial.print(mqttClient.state());
+		Serial.println(F(" try again in 5 seconds"));
+		
+	}
+}
+
+void sendMqtt(String sensor, String value){
+	mqttClient.publish((String(outTopic) + sensor).c_str(), value.c_str());
 }
